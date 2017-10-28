f6438373ddeadd99cb3ccba6aacc2ebae4713d01
allanpacheco <31258913+allanpacheco@users.noreply.github.com>
2017-10-28 11:07:38 -0500

    Mi primer cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'BecaAngular Everis';
+  colores = ['blanco','azul', 'rojo'];
+  mostrarListaColores = false;
+  funcion_enviar (valorColor){
+    console.log(valorColor.value);
+    this.colores.push(valorColor.value);
+    valorColor.vale="";
+    return false;
+  }
+
+funcion_Mostrar(){
+this.mostrarListaColores=!this.mostrarListaColores;
+
 }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NuevocomponenteComponent } from './nuevocomponente/nuevocomponente.component';
+import { CommentsComponent } from './comments/comments.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevocomponenteComponent,
+    CommentsComponent,
+    AlbumsComponent,
+    TodosComponent,
+    UsersComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule
